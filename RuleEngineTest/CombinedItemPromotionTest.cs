8de0dd80dc1921c0e7c9ebe2d6c69ce25582d5fd
Pankaj Chahar <pankaj.chahar@siemens-healthineers.com>
2022-03-14 13:35:05 +0530

    Added some additiona test for active promotions

--- RuleEngineTest/CombinedItemPromotionTest.cs
+++ RuleEngineTest/CombinedItemPromotionTest.cs
@@ -45,6 +45,38 @@ namespace RuleEngineTest
         }
 
         [Fact]
+        public void TestApplyPromotion_WithCombinedItemPromotionSenarioWithDiffFormat_ReturnApplyPromotionPrice()
+        {
+            string inputPromo = "C & D for 30";
+            var inventory = new Inventory();
+            var cartItem = new CartItem();
+            cartItem.IsPromotionApplied = false;
+            cartItem.Item = new SKUItem("C", 20);
+            cartItem.TotalPrice = 20;
+
+            var cartItem1 = new CartItem();
+            cartItem1.IsPromotionApplied = false;
+            cartItem1.Item = new SKUItem("D", 15);
+            cartItem1.TotalPrice = 15;
+
+            inventory.AddSKUitem(new SKUItem("C", 20));
+            inventory.AddSKUitem(new SKUItem("D", 15));
+
+            inventory.AddPromotion(inputPromo);
+
+            inventory.AddItemToCart("C");
+            inventory.AddItemToCart("D");
+
+            Assert.Equal(Convert.ToDecimal(35), inventory._cart.TotalPrice());
+
+
+            inventory.Checkout();
+
+            Assert.Equal(Convert.ToDecimal(30), inventory._cart.TotalPrice());
+
+        }
+
+        [Fact]
         public void TestApplyPromotion_WithCombinedItemPromotionWithFixedPriceZero_ThrowPromotionRuleEngineException()
         {
            
--- RuleEngineTest/IndividualItemPromotionTest.cs
+++ RuleEngineTest/IndividualItemPromotionTest.cs
@@ -11,7 +11,7 @@ namespace RuleEngineTest
     public class IndividualItemPromotionTest
     {
         [Fact]
-        public void TestApplyPromotion_WithIndividualItemPromotino_ReturnExpectedReducedPrice()
+        public void TestApplyPromotion_WithIndividualItemPromotinoGivenSenario1_ReturnExpectedReducedPrice()
         {
             string inputPromo = "3 A for 130";
             var inventory = new Inventory();
@@ -40,6 +40,90 @@ namespace RuleEngineTest
            
         }
 
+        [Fact]
+        public void TestApplyPromotion_WithIndividualItemPromotinoGivenSenario2withDiffFormat_ReturnExpectedReducedPrice()
+        {
+            string inputPromo = "2 B for 45";
+            var inventory = new Inventory();
+            var cartItem = new CartItem();
+            cartItem.IsPromotionApplied = false;
+            cartItem.Item = new SKUItem("B", 30);
+            cartItem.TotalPrice = 30;
+
+            //var cart = new Cart() { cartItems = { cartItem, cartItem, cartItem } };
+            inventory.AddSKUitem(new SKUItem("B", 30));
+            inventory.AddSKUitem(new SKUItem("B", 30));
+
+            inventory.AddItemToCart("B");
+            inventory.AddItemToCart("B");
+
+
+            Assert.Equal(Convert.ToDecimal(60), inventory._cart.TotalPrice());
+
+            inventory.AddPromotion(inputPromo);
+            inventory.Checkout();
+
+            Assert.Equal(Convert.ToDecimal(45), inventory._cart.TotalPrice());
+
+        }
+
+        [Fact]
+        public void TestApplyPromotion_WithIndividualItemPromotinoGivenSenario1withDiffFormat_ReturnExpectedReducedPrice()
+        {
+            string inputPromo = "3 of A's for 130";
+            var inventory = new Inventory();
+            var cartItem = new CartItem();
+            cartItem.IsPromotionApplied = false;
+            cartItem.Item = new SKUItem("A", 50);
+            cartItem.TotalPrice = 50;
+
+            //var cart = new Cart() { cartItems = { cartItem, cartItem, cartItem } };
+            inventory.AddSKUitem(new SKUItem("A", 50));
+            inventory.AddSKUitem(new SKUItem("A", 50));
+            inventory.AddSKUitem(new SKUItem("A", 50));
+
+            inventory.AddItemToCart("A");
+            inventory.AddItemToCart("A");
+            inventory.AddItemToCart("A");
+
+
+
+            Assert.Equal(Convert.ToDecimal(150), inventory._cart.TotalPrice());
+
+            inventory.AddPromotion(inputPromo);
+            inventory.Checkout();
+
+            Assert.Equal(Convert.ToDecimal(130), inventory._cart.TotalPrice());
+
+        }
+
+        [Fact]
+        public void TestApplyPromotion_WithIndividualItemPromotinoGivenSenario2_ReturnExpectedReducedPrice()
+        {
+            string inputPromo = "2 of B's for 45";
+            var inventory = new Inventory();
+            var cartItem = new CartItem();
+            cartItem.IsPromotionApplied = false;
+            cartItem.Item = new SKUItem("B", 30);
+            cartItem.TotalPrice = 30;
+
+            //var cart = new Cart() { cartItems = { cartItem, cartItem, cartItem } };
+            inventory.AddSKUitem(new SKUItem("B", 30));
+            inventory.AddSKUitem(new SKUItem("B", 30));
+
+            inventory.AddItemToCart("B");
+            inventory.AddItemToCart("B");
+
+
+            Assert.Equal(Convert.ToDecimal(60), inventory._cart.TotalPrice());
+
+            inventory.AddPromotion(inputPromo);
+            inventory.Checkout();
+
+            Assert.Equal(Convert.ToDecimal(45), inventory._cart.TotalPrice());
+
+        }
+
 
         [Fact]
         public void TestApplyPromotion_WithIndividualItemPromotionWithFixedPriceZero_ThrowsPromotionRuleEngineException()
@@ -61,6 +145,36 @@ namespace RuleEngineTest
             inventory.AddItemToCart("A");
 
             Assert.Throws<PromotionRuleEngineException>(() => individualPromotion.ApplyPromotion(cart));
+
+        }
+
+        [Fact]
+        public void TestApplyPromotion_SenarioA()
+        {
+            //string inputPromo = "3 A for 130";
+            var inventory = new Inventory();
+            var cartItem = new CartItem();
+            cartItem.IsPromotionApplied = false;
+            cartItem.Item = new SKUItem("A", 50);
+            cartItem.TotalPrice = 50;
+
+            //var cart = new Cart() { cartItems = { cartItem, cartItem, cartItem } };
+            inventory.AddSKUitem(new SKUItem("A", 50));
+            inventory.AddSKUitem(new SKUItem("A", 50));
+            inventory.AddSKUitem(new SKUItem("A", 50));
+
+            inventory.AddItemToCart("A");
+            inventory.AddItemToCart("A");
+            inventory.AddItemToCart("A");
+
+
+
+            Assert.Equal(Convert.ToDecimal(150), inventory._cart.TotalPrice());
+
+            inventory.AddPromotion(inputPromo);
+            inventory.Checkout();
+
+            Assert.Equal(Convert.ToDecimal(130), inventory._cart.TotalPrice());
 
         }
     }
